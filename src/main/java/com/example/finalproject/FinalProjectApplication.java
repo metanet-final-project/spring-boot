6e13369f97ba030f1b31a19b087f8bbe81199031
LEE <evisahah@gmail.com>
2023-04-11 11:15:19 +0900

    woo

--- src/main/java/com/example/finalproject/FinalProjectApplication.java
+++ src/main/java/com/example/finalproject/FinalProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
